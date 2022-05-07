74133c91c94324e681969005fc0dd37022a56825
anitaloggg <huichen.lo@gmail.com>
2022-05-07 16:24:21 +0800

    main

---
+++ Controllers/MeetingRoomsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mod09API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mod09API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MeetingRoomsController : ControllerBase
+    {
+        private readonly MeetingRoomContext _context;
+
+        public MeetingRoomsController(MeetingRoomContext context)
+        {
+            _context = context;
+        }
+        // GET: MeetingRooms  
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MeetingRoom>>> GetMeetingRooms()
+        {
+            return await _context.MeetingRooms.ToListAsync();
+        }
+
+        // GET: api/MeetingRooms/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MeetingRoom>> GetMeetingRoom(int id)
+        {
+            var meetingRoom = await _context.MeetingRooms.FindAsync(id);
+            if (meetingRoom == null)
+            {
+                return NotFound();
+            }
+            return meetingRoom;
+        }
+
+
+        // POST api/<MeetingRoomsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MeetingRoomsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MeetingRoomsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Migrations/20220507080234_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mod09API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MeetingRooms",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    Projector = table.Column<bool>(type: "bit", nullable: true),
+                    TV = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetingRooms", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MeetingRooms");
+        }
+    }
+}
