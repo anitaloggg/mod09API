b0cdcca5b77d2a0d1db25f422b88703cd372bf86
anitaloggg <huichen.lo@gmail.com>
2022-05-07 15:50:12 +0800

    main

---
+++ Model/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mod09API.Model
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(MeetingRoomContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.MeetingRooms.Any()) return;
+            MeetingRoom[] mrList = {
+                new MeetingRoom
+                {
+                    Name = "馬德里",
+                    Size = 10, Projector = true, TV=false
+                }, new MeetingRoom
+                {
+                    Name = "里約",
+                    Size = 100, Projector = true, TV=true
+                } , new MeetingRoom
+                {
+                    Name = "哥多華",
+                    Size = 200, Projector = true, TV=true
+                },  new MeetingRoom
+                {
+                    Name = "巴塞隆納",
+                    Size = 50, Projector = false, TV=true
+                }};
+            context.MeetingRooms.AddRange(mrList);
+            context.SaveChanges();
+        }
+
+    }
+}
